81b14f99af16fe6f295805d83b36a5c71d17f827
徐亮亮 <1626769688@qq.com>
2021-12-11 18:23:59 +0800

    添加对 RHEL/CentOS 7.5 的支持

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -1049,7 +1049,7 @@ void rtw_cfg80211_indicate_connect(_adapter *padapter)
 #ifdef CONFIG_P2P
 	struct wifidirect_info *pwdinfo = &(padapter->wdinfo);
 #endif
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0) || RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7, 8)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0) || RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7, 5)
 	struct cfg80211_roam_info roam_info ={};
 #endif
 
@@ -1122,7 +1122,7 @@ check_bss:
 		notify_channel = ieee80211_get_channel(wiphy, freq);
 		#endif
 
-		#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0) || RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7, 8)
+		#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0) || RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7, 5)
 		roam_info.bssid = cur_network->network.MacAddress;
 		roam_info.req_ie = pmlmepriv->assoc_req + sizeof(struct rtw_ieee80211_hdr_3addr) + 2;
 		roam_info.req_ie_len = pmlmepriv->assoc_req_len - sizeof(struct rtw_ieee80211_hdr_3addr) - 2;
@@ -2326,7 +2326,7 @@ enum nl80211_iftype {
 static int cfg80211_rtw_change_iface(struct wiphy *wiphy,
 				     struct net_device *ndev,
 				     enum nl80211_iftype type,
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)) && RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(7, 8)
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)) && RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(7, 5)
 				     u32 *flags,
 #endif
 				     struct vif_params *params)
@@ -4739,7 +4739,7 @@ static int
 		unsigned char name_assign_type,
 		#endif
 		enum nl80211_iftype type,
-		#if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0) && RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(7, 8))
+		#if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0) && RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(7, 5))
 		u32 *flags,
 		#endif
 		struct vif_params *params)
@@ -9536,7 +9536,7 @@ static void rtw_cfg80211_preinit_wiphy(_adapter *adapter, struct wiphy *wiphy)
 #endif
 
 #if defined(CONFIG_PM) && (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 0, 0) && \
-			   LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0) && RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(7, 8))
+			   LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0) && RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(7, 5))
 	wiphy->flags |= WIPHY_FLAG_SUPPORTS_SCHED_SCAN;
 #ifdef CONFIG_PNO_SUPPORT
 	wiphy->max_sched_scan_ssids = MAX_PNO_LIST_COUNT;
